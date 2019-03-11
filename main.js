2c7ca1aaeab7007fcb34917ea1844a5e2fff8244
Gabriella Gintare Rozenaite <34273311+Gabriella-roze@users.noreply.github.com>
2019-03-11 19:38:00 +0100

    add font styling

--- main.js
+++ main.js
@@ -8,17 +8,28 @@ const loading = {
     start: () => document.querySelector('.loading-overlay').style.display = 'initial',
     stop: () => document.querySelector('.loading-overlay').style.display = 'none',
 };
-const form = document.querySelector("form");
-const btnSubmitToday = form.elements.submit;
+
+const formToday = document.querySelector("#form__today");
+const formWeek = document.querySelector("#form__week");
+const formMonth = document.querySelector("#form__month");
+const formYear = document.querySelector("#form__year");
+
+const btnSubmitToday = formToday.elements.submit;
+const btnSubmitWeek = formWeek.elements.submit;
+const btnSubmitMonth = formMonth.elements.submit;
+const btnSubmitYear = formYear.elements.submit;
 
 const todayParent = document.querySelector("#form__today>.tasks__box");
 const weekParent = document.querySelector("#form__week>.tasks__box");
 const monthParent = document.querySelector("#form__month>.tasks__box");
+const yearParent = document.querySelector("#form__year>.tasks__box");
+
 const template = document.getElementById("tpl__toDo").content;
 
 let todoToday = [];
 let todoWeek = [];
 let todoMonth = [];
+let todoYear = [];
 
 // INIT
 window.addEventListener("load", init());
@@ -31,6 +42,7 @@ async function init() {
     todoToday = todos.filter(todo => todo.timeline === "today");
     todoWeek = todos.filter(todo => todo.timeline === "week");
     todoMonth = todos.filter(todo => todo.timeline === "month");
+    todoYear = todos.filter(todo => todo.timeline === "year");
 
     loading.stop();
     console.log('todos: ', todos);
@@ -41,6 +53,7 @@ function renderTodo(todos) {
     todayParent.innerHTML = '';
     weekParent.innerHTML = '';
     monthParent.innerHTML = '';
+    yearParent.innerHTML = '';
 
     todos.forEach(todo => {
         let tplClone = template.cloneNode(true);
@@ -59,8 +72,10 @@ function renderTodo(todos) {
             todayParent.appendChild(tplClone);
         } else if (todo.timeline === "week") {
             weekParent.appendChild(tplClone);
-        } else {
+        } else if (todo.timeline === "month"){
             monthParent.appendChild(tplClone);
+        } else {
+            yearParent.appendChild(tplClone);
         }
     });
 }
@@ -81,18 +96,67 @@ function getTodos(obj) {
         .then(res => res.json())
 }
 
+// BTNS EVENT LISTENERS TO SUBMIT NEW TASK
 btnSubmitToday.addEventListener("click", (e) => {
     e.preventDefault();
-    const newTodo = {
-        isCompleted: false,
-        task: form.elements.newToday.value,
-        timeline: "today"
+    addNewTask("today");
+});
+btnSubmitWeek.addEventListener("click", (e) => {
+    e.preventDefault();
+    addNewTask("week");
+});
+btnSubmitMonth.addEventListener("click", (e) => {
+    e.preventDefault();
+    addNewTask("month");
+});
+btnSubmitYear.addEventListener("click", (e) => {
+    e.preventDefault();
+    addNewTask("year");
+});
+
+// ADD NEW TODO
+function addNewTask(timeline) {
+    // let form = ${form + timeline.charAt(0).toUpperCase() + timeline.slice(1)};
+    let newTodo;
+    if (timeline === "today") {
+        newTodo = {
+            isCompleted: false,
+            task: formToday.elements.newToday.value,
+            timeline: "today"
+        }
+        formToday.elements.newToday.value = "";
+    } else if (timeline === "week") {
+        newTodo = {
+            isCompleted: false,
+            task: formWeek.elements.newWeek.value,
+            timeline: "week"
+        }
+        formWeek.elements.newWeek.value = "";
+
+    } else if (timeline === "month") {
+        newTodo = {
+            isCompleted: false,
+            task: formMonth.elements.newMonth.value,
+            timeline: "month"
+        }
+        formMonth.elements.newMonth.value = "";
+    } else {
+        newTodo = {
+            isCompleted: false,
+            task: formYear.elements.newYear.value,
+            timeline: "year"
+        }
+        formYear.elements.newYear.value = "";
     }
-    form.elements.newToday.value = "";
+    
+   
     console.log("New Todo", newTodo);
     postTodo(newTodo)
         .then(res => init())
-});
+}
+
+
+
 
 
 
